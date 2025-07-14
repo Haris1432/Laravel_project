42f5aa2c2a67e815c138c382e4515eba05e9864b
harisjutt <harisjutt0554@gmail.com>
2025-07-14 16:37:44 +0500

    Add TitleController with index Method

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\TitleController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/hello', [HelloController::class, 'index']);
+Route::get('/title', [TitleController::class, 'index']);
